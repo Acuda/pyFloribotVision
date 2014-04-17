1334b6fa8432a9926d877d9e32c8eefd7e639df2
Acuda <eistel@gmail.com>
2014-04-17 15:57:56 +0200

    value output to stdout

---
+++ pyfloribotvision/plugins/python/DataToStdout.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+#
+#Author: Björn Eistel
+#Contact: <eistel@gmail.com>
+#
+# THIS SOURCE-CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED. IN NO 
+# EVENT WILL THE AUTHOR BE HELD LIABLE FOR ANY DAMAGES ARISING FROM THE USE OF THIS SOURCE-CODE. 
+# USE AT YOUR OWN RISK.
+
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-
+#
+#Author: Björn Eistel
+#Contact: <eistel@gmail.com>
+#
+# THIS SOURCE-CODE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED. IN NO
+# EVENT WILL THE AUTHOR BE HELD LIABLE FOR ANY DAMAGES ARISING FROM THE USE OF THIS SOURCE-CODE.
+# USE AT YOUR OWN RISK.
+
+
+from pyfloribotvision.types.StringListType import StringListType
+from .. BaseModule import BaseModule
+import time
+import logging
+
+
+class DataToStdout(BaseModule):
+
+    configParameter = [
+        StringListType('inputData', input=True),
+    ]
+
+    def __init__(self, **kwargs):
+        super(DataToStdout, self).__init__(**kwargs)
+        self.log = logging.getLogger(__name__)
+        self.log.debug('logging started')
+
+    def externalCall(self):
+        for data in self.inputData.value:
+            print data, self.inputData.data[data]
